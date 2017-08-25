b3462b1a1f4f195720c54effbdfd8a50ee4ba0c5
lgybetter <437675103@qq.com>
2017-08-26 02:56:00 +0800

    add the url-loader and sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+var path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
 module.exports = {
   devtool: 'eval-source-map',
   entry: __dirname + '/src/js/index.js',
@@ -10,11 +16,23 @@ module.exports = {
       {
         test: /.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
       },
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000
+        }
       }
     ]
   },
